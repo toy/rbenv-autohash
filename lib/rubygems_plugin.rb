4a3fdd27be327e9f6bee4efd71af6045db171257
Ivan Kuchin <tadump+git@gmail.com>
2014-09-26 02:23:13 +0200

    run rehash once at_exit

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,8 +1,16 @@
+do_rehash = false
+
 hook = lambda do |installer|
   if installer.spec.executables.any?
-    system "rbenv", "rehash"
+    do_rehash = true
   end
 end
 
 Gem.post_install(&hook)
 Gem.post_uninstall(&hook)
+
+at_exit do
+  if do_rehash
+    system 'rbenv', 'rehash'
+  end
+end
